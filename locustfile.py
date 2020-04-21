c2eedb1fe40b8dec43f923ef1c2be17feee54306
ed36cuir <ed36cuir@hotmail.com>
2020-04-21 03:48:02 +0000

    fixed loadfile

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,22 @@
-from locust import HttpLocust, TaskSet, between
+import random
+from locust import HttpLocust, task, between
 
-def index(l):
-    l.client.get("/")
-    
-class UserBehavior(TaskSet):
-    task = {index:1}
-    
 class WebsiteUser(HttpLocust):
-    task_set = UserBehavior
-    wait_time = between(5.0, 9.0)
+    wait_time = between(5, 9)
+
+    @task(2)
+    def index(self):
+        self.client.get("/")
+        self.client.get("/ajax-notifications/")
+
+    @task(1)
+    def view_post(self):
+        post_id = random.randint(1, 10000)
+        self.client.get("/post?id=%i" % post_id, name="/post?id=[post-id]")
+
+    def on_start(self):
+        """ on_start is called when a Locust start before any task is scheduled """
+        self.login()
+
+    def login(self):
+        self.client.post("/login", {"username":"ellen_key", "password":"education"})
